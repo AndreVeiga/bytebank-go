f2cfdc6ca49ee7f2ca6b706a79ed5ffa2a26b0af
AndreVeiga <soutomachado1@gmail.com>
2022-12-27 11:52:13 +0044

    first commit

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	titular := "Elton"
+	numeroAgencia := 589
+	numeroConta := 123456
+	saldo := 0.0
+
+	fmt.Println(titular, numeroAgencia, numeroConta, saldo)
+
+}
